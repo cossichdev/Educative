40b3f14cb2c819953b44ab5f2fc6b257d6dc6c9a
Cossich <cossichglauco@gmail.com>
2022-10-22 00:03:41 -0600

    gitignore

---
+++ Full Speed Python/strings.py
@@ -0,0 +1,6 @@
+s = (input("Ingresa a string): ")) # se pide al usuario que ingrese una string
+
+s = s[:1] # se toma el primer caracter de la string
+s=  s[0] # se toma el primer caracter de la string
+s = s[1:] # se toma el resto de la string
+print(s)
