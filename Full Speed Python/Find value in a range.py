b04a669cb2ebbb5ead0ae0f5c710be9c02a97e63
Cossich <cossichglauco@gmail.com>
2022-10-21 20:35:51 -0600

    Buscar entre un rango

--- Full Speed Python/Find value in a range.py
+++ Full Speed Python/Find value in a range.py
@@ -3,10 +3,7 @@ x = int(input("Enter a number (X): "))  # se pide al usuario que ingrese un nume
 y = int(input("Enter a number (Y): "))  # se pide al usuario que ingrese un numero
 
 def inRange(x, y): # funcion que verifica si un numero esta en un rango
-  
-  result = (x < 1/3 < y) # si el numero esta entre 1/3 y y, retorna True
-  
-  return result # retorna el resultado
+  return (x < 1/3 < y)  # si el numero esta en el rango, retorna True
 
 print(inRange(x, y)) # llamada a la funcion
 
